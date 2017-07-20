cb9e5f93829de5e43e18ded4a24e341cd3cb2886
Salih Tuc <salihtuc@hotmail.com>
2017-07-20 13:50:46 +0300

    Classes updated

--- src/Function.java
+++ src/Function.java
@@ -4,6 +4,7 @@
  * 
  * */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,35 +23,35 @@ public class Function implements DifferentiableFunction {
 	public FunctionValues getValues(double[] point) {
 
 //		System.out.println("Points:");
-//		Main.printDoubleArray(point);
+		//Main.printDoubleArray(point);
 
 		return new FunctionValues(functionValue(point), gradient(point));
 	}
 
 	public double functionValue(double[] iterWeights) {
-
-		for (HashMap<Integer, HashMap<Integer, Node>> globalMap : Main.sentenceGlobals) {
+		
+		for (HashMap<Integer, HashMap<Integer, Node>> globalMap : Main.sentenceGlobals) {	// Iterate for all sentences
 			int counter = 0;
 			double sumNum = 0.0;
 			double sumDenom = 0.0;
-			for (HashMap<Integer, Node> map : globalMap.values()) {
-				for (Node endState : Main.returnEndStates(map)) {
+			for (HashMap<Integer, Node> map : globalMap.values()) {	// Each sentence's lattices
+				for (Node endState : Main.returnEndStates(map)) {	// Nodes of each lattice
 					if (counter == 0) {
-						sumNum += addListValues(endState.tagScores);
+						sumNum += addListValues(endState.alpha);	// Original lattice's score
 					} else {
-						sumDenom += addListValues(endState.tagScores);
+						sumDenom += addListValues(endState.alpha);	// Negative samples' scores
 					}
 				}
 			}
-
-			functionValue += (sumNum - sumDenom);
+			functionValue += (sumNum - sumDenom);	// Each sentence's scores added.
 		}
 
+		// Regularization
 		for (double weight : iterWeights) {
 			functionValue -= LAMBDA_EM * Math.pow(weight, 2);
 		}
-		functionValue *= -1;
-
+		functionValue *= -1;	// -1 because of minimizing
+		
 		return functionValue;
 	}
 
@@ -66,13 +67,14 @@ public class Function implements DifferentiableFunction {
 				double denom = 0;
 				for (HashMap<Integer, HashMap<Integer, Node>> globalMap : Main.sentenceGlobals) {
 					for (int a = 0; a < globalMap.size(); a++) {
-						for (int k = 0; k < globalMap.get(a).size() - 1; k++) {
-							for (int k1 : globalMap.get(a).get(k).next) {
-								Node n2 = globalMap.get(a).get(k1);
+						HashMap<Integer, Node> lattice = globalMap.get(a);
+						for (int k = 0; k < lattice.size() - 1; k++) {
+							for (int k1 : lattice.get(k).next) {
+								Node n2 = lattice.get(k1);
 								for (int k2 = 0; k2 < Main.tagSize; k2++) {
-									denom += p(i, k2, globalMap.get(a).get(k), n2);
+									denom += p(i, k2, lattice.get(k), n2);
 								}
-								num += p(i, j, globalMap.get(a).get(k), n2);
+								num += p(i, j, lattice.get(k), n2);
 							}
 
 						}
@@ -83,7 +85,32 @@ public class Function implements DifferentiableFunction {
 
 		}
 
-		/* re-estimation of emission probabilities */
+//		/* re-estimation of emission probabilities */
+//		
+//		for (int i = 0; i < Main.tagSize; i++) {
+//			double num = 0;
+//			double denom = 0;
+//			
+//			for (HashMap<Integer, HashMap<Integer, Node>> globalMap : Main.sentenceGlobals) {
+//				for (int a = 0; a < globalMap.size(); a++) {
+//					ArrayList<String> uniqueWords = Main.returnUniqueWords(globalMap.get(a));
+//					for (int k = 0; k < globalMap.get(a).size(); k++) {
+//						for (int j = 0; j < uniqueWords.size(); j++) {
+//							
+//							double g = gamma(i, globalMap.get(a).get(k));
+//							num += g * (uniqueWords.get(j).equals(globalMap.get(a).get(k).word) ? 1 : 0);
+//							denom += g;
+//							grademissionProbabilities.put(Main.tagList.get(i) + "-" + uniqueWords.get(j),
+//									divide(num, denom));
+//						}
+//					}
+//				}
+//
+//			}
+//		}
+
+		
+/* re-estimation of emission probabilities */
 		
 		for (int i = 0; i < Main.tagSize; i++) {
 			for (int j = 0; j < Main.allWords.size(); j++) {
@@ -108,6 +135,9 @@ public class Function implements DifferentiableFunction {
 
 		grad = Main.createWeightsArray(gradtransitionProbabilities, grademissionProbabilities);
 
+		for (int i = 0; i < grad.length; i++)
+			grad[i] *= -1;
+			
 		for (int i = 0; i < grad.length; i++)
 			grad[i] += 2 * LAMBDA_EM * iterWeights[i];
 
