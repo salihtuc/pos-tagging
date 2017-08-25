8375b5362670e649e9c13025f5d81fe8486a82c2
necvabolucu <necva.bolucu@hacettepe.edu.tr>
2017-08-25 19:56:54 +0300

    Update bhmm.py

--- bhmm.py
+++ bhmm.py
@@ -14,17 +14,19 @@ class BHMM(object):
         self.frequencyEmit = defaultdict(float)
         self.frequencyTransExp = defaultdict(float)
         self.frequencyEmitExp = defaultdict(float)
-        self.tag=["t1","t2","t3","t4","t5","t6","t7","t8","t9","t10","t11","t12","<s>"]
-    def __change_count(self,matrix, x, y, i):
+        
+        self.minTrans=float(-8.390173385616183e-07)
+        self.minEmit=float(-2.765386569039885e-10)
+    def __change_count(self,matrix, x, y, i,a):
 #        print(x,y)
-        matrix["%s|%s" % (x, y)] += i
-        matrix["%s" % x] += i
+        matrix["%s|%s" % (x, y)] += (i +a)
+        matrix["%s" % x] +=(i +a)
 #        print( matrix["%s|%s" % (x, y)], matrix["%s" % x])
     
-    def __change_countExp(self,matrix, x, y, i):
+    def __change_countExp(self,matrix, x, y, a,i):
     #        print(x,y)
-            matrix["%s|%s" % (x, y)] += exp(i)
-            matrix["%s" % x] +=exp( i  )
+            matrix["%s|%s" % (x, y)] += exp(i-a+0.0001)
+            matrix["%s" % x] +=exp( i -a+0.0001 )
                   
     def __get_value(self,matrix, x,y):
 #        print(x, y)
@@ -38,19 +40,24 @@ class BHMM(object):
         count=1
         file=open("result.txt")
         file1=open("out.txt","w")
-        file2=open("outExp.txt","w")
+        
         for line in file.readlines():
+            
             splitted=line.strip("\n").strip().split()
             splittedValue=splitted[0].split("-",1)
+
             if count<170:
 #            print(splittedValue)
-                self.__change_count(self.frequencyTrans, splittedValue[0], splittedValue[1],float(splitted[1]))
-                self.__change_countExp(self.frequencyTransExp, splittedValue[0], splittedValue[1],float(splitted[1]))
+                self.__change_count(self.frequencyTrans, splittedValue[0], splittedValue[1], float(splitted[1]),(-1*self.minTrans+0.001))
+#                self.__change_countExp(self.frequencyTransExp, splittedValue[0], splittedValue[1],self.minTrans,float(splitted[1]))
             else:
-                self.__change_count(self.frequencyEmit, splittedValue[0], splittedValue[1],float(splitted[1]))
-                self.__change_countExp(self.frequencyEmitExp, splittedValue[0], splittedValue[1],float(splitted[1]))
+                self.__change_count(self.frequencyEmit, splittedValue[0], splittedValue[1],float(splitted[1]),(-1*self.minEmit+0.001))
+#                self.__change_countExp(self.frequencyEmitExp, splittedValue[0], splittedValue[1],self.minEmit,float(splitted[1]))
             count=count+1
-            
+
+        print(min([y for x,y in self.frequencyTrans.items() if "|" in x]),max([y for x,y in self.frequencyTrans.items() if "|" in x]))
+        print(min([y for x,y in self.frequencyEmit.items() if "|" in x]),max([y for x,y in self.frequencyEmit.items() if "|" in x]))
+
 #        print(len(self.frequency.keys()))
         for key in self.frequencyTrans.keys():
             if "|" in key:
@@ -58,7 +65,7 @@ class BHMM(object):
 #                print(key)
 #                print(key,self.__get_value(self.frequency,splittedValue[0],splittedValue[1]))
                 file1.write(key+" "+str(self.__get_value(self.frequencyTrans,splittedValue[0],splittedValue[1]))+"\n")
-                file2.write(key+" "+str(self.__get_value(self.frequencyTransExp,splittedValue[0],splittedValue[1]))+"\n")
+#                file2.write(key+" "+str(self.__get_value(self.frequencyTransExp,splittedValue[0],splittedValue[1]))+"\n")
                 
                 
         for key in self.frequencyEmit.keys():
@@ -67,7 +74,9 @@ class BHMM(object):
         #                print(key)
         #                print(key,self.__get_value(self.frequency,splittedValue[0],splittedValue[1]))
                 file1.write(key+" "+str(self.__get_value(self.frequencyEmit,splittedValue[0],splittedValue[1]))+"\n")
-                file2.write(key+" "+str(self.__get_value(self.frequencyEmitExp,splittedValue[0],splittedValue[1]))+"\n")
+#                file2.write(key+" "+str(self.__get_value(self.frequencyEmitExp,splittedValue[0],splittedValue[1]))+"\n")
+                
+#        print(min(self.frequencyTrans.values()),min(self.frequencyEmit.values()))
 
         
     
